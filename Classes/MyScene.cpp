5cddfeadc97ad727c2f5cfb96575e624f0f77ea0
RootReturn0 <1121149171@qq.com>
2018-05-24 04:26:59 +0800

    MyScene.cpp

---
+++ Classes/MyScene.cpp
@@ -0,0 +1,17 @@
+
+#include "MyScene.h"
+	USING_NS_CC;
+	Scene* MyWorld::createScene()
+	{
+		auto scene = Scene::create();
+		auto layer = MyWorld::create();
+		scene->addChild(layer);
+		return scene;
+	}
+	bool MyWorld::init()
+	{
+		if (!Layer::init())
+			return false;
+		//add scenes here
+		return true;
+	}
